8aa99c9f9c8c3bae74328e13f008be3e1b3ca834
GasparOctavio <gasparcorreavergara@gmail.com>
2020-06-08 20:57:22 -0400

    Updates

---
+++ precache-manifest.6d4dab5604be4862d9a5c522ad2da7e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab22bdf77b46e4854da9c304263a0af",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "bb8a12c6c74eaa6800ab",
+    "url": "/pokedex/static/css/main.10a202a4.chunk.css"
+  },
+  {
+    "revision": "0bea1ed9fd059e105b5b",
+    "url": "/pokedex/static/js/2.b2d682db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.b2d682db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8a12c6c74eaa6800ab",
+    "url": "/pokedex/static/js/main.f2906f46.chunk.js"
+  },
+  {
+    "revision": "3720d53d7f3565cf8d9c",
+    "url": "/pokedex/static/js/runtime-main.115cfb4e.js"
+  },
+  {
+    "revision": "692c37377cd1e0f4850f47c7c0148426",
+    "url": "/pokedex/static/media/VT323-Regular.692c3737.ttf"
+  }
+]);
